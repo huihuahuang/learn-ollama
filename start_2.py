00d57635e8b73aa4807b8b8bea47e2dd627b60f5
Huihua Huang <addiehuanghuihua@gmail.com>
2025-09-30 23:02:32 -0400

    Create and delete customerized model with ollama python module

--- start_2.py
+++ start_2.py
@@ -27,3 +27,19 @@ res2 = ollama.chat(
 for line in res2:
     print(line["message"]["content"], end="", flush=True)
 
+# Create a new model with modelfile
+# file = """
+# FROM llama3.2
+
+# SYSTEM "You are a very smart assistant who knows everything about the ocean. Be succinct and informative."
+
+# PARAMETER temperature 0.1
+# """
+
+
+# ollama.create(model="knowitall", modelfile=file)
+
+# res3 = ollama.generate(model="knowitall", prompt="why is the ocean salty?")
+
+# Delete model
+# ollama.delete("knowitall")
